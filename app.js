bf344f9037130560b2f1c2138c198fce510a3cf2
nowamey <nowamey100@gmail.com>
2022-09-13 15:38:49 -0400

    lr

--- app.js
+++ app.js
@@ -67,11 +67,66 @@ document.addEventListener('DOMContentLoaded',() => {
 
       //make the tetromino move down every second
       timerId = setInterval(moveDown,1000)
+
+      //assign functions to keyCodes
+      function control(e){
+        if(e.keyCode === 37){
+            moveLeft()
+        }else if (e.keyCode === 38){
+            //rotate
+        }else if (e.keyCode === 39){
+            moveRight()
+        }else if (e.keyCode === 40){
+            moveDown()
+        }
+      }
+      document.addEventListener('keyup',control)
+
       
       //move donw function
       function moveDown(){
         undraw()
         currentPosition += width
+        draw()
+        freeze()
+      }
+
+      function freeze(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+
+            random = Math.floor(Math.random() *theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+
+        }
+      }
+
+
+      function moveLeft(){
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index=>squares[currentPosition+index].classList.contains('taken'))) {
+            currentPosition +=1
+        }
+
+        draw()
+      }
+    
+      //move the tetromino right unless it is at the edge or blockage
+      function moveRight(){
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index)% width ===width -1 )
+
+        if(!isAtRightEdge) currentPosition +=1
+
+        if(current.some(index=>squares[currentPosition+index].classList.contains('taken'))){
+            currentPosition -=1
+        }
         draw()
       }
 })
