0811755bfd3417acb8c53fc09b60defa699ade1d
nowamey <nowamey100@gmail.com>
2022-09-13 11:46:36 -0400

    early js work

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded',() => {
-
+    const width = 10
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const grid = document.querySelector('.grid')
 })
